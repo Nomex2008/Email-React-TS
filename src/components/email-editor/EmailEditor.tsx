99d38c892dfe6ffe48333d30548523a46ddf9d7b
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-03-19 15:00:26 +0200

    Start 2
    Add components, css style and app design.

---
+++ src/components/email-editor/EmailEditor.tsx
@@ -0,0 +1,10 @@
+import styles from './EmailEditor.module.css'
+
+export function EmailEditor() {
+  return (
+    <>
+      <h1>Email editor</h1>
+      <div className={styles.card}></div>
+    </>
+  )
+}
---
+++ src/components/email-list/EmailList.jsx
@@ -0,0 +1,7 @@
+export const EmailList = () => {
+    return ( 
+        <>
+            EmailList
+        </> 
+    );
+}
