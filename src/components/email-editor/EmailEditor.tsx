7aadace05ed2162c1e3b721886ff103123477655
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-03-19 22:40:55 +0200

    Commit 1 (editorial-layout )
    Editorial layout.

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,10 +1,28 @@
-import styles from './EmailEditor.module.css'
+import { Bold, Eraser, Italic, Underline } from 'lucide-react'
+import styles from './EmailEditor.module.scss'
 
 export function EmailEditor() {
   return (
-    <>
+    <div>
       <h1>Email editor</h1>
-      <div className={styles.card}></div>
-    </>
+      <div className={styles.card}>
+
+        <div className={styles.editor}>
+          Hey, Alex
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos accusamus reiciendis tempore repellendus veritatis sapiente voluptate ratione distinctio assumenda at.
+        </div>
+
+        <div className={styles.actions}>
+          <div className={styles.tools}>
+              <button><Eraser size={17}/></button>
+              <button><Bold size={17}/></button>
+              <button><Italic size={17}/></button>
+              <button><Underline size={17}/></button>
+          </div>
+          <button>Send now</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
